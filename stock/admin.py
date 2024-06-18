116695f08149e07dd15638bcb0470cc07a04ea52
Omar Flores <sazardev@gmail.com>
2024-06-18 16:19:39 -0700

    feat: Add Order and Transaction models to stock admin

--- stock/admin.py
+++ stock/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
-from .models import Product, Category, Manufacturer, Supplier
+from .models import Order, Product, Category, Manufacturer, Supplier, Transaction
 
 admin.site.register(Product)
 admin.site.register(Category)
 admin.site.register(Manufacturer)
 admin.site.register(Supplier)
+admin.site.register(Order)
+admin.site.register(Transaction)
