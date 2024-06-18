03b6fe0dbced8de18338e3652df36f6b18eabfa9
Omar Flores <sazardev@gmail.com>
2024-06-18 16:20:07 -0700

    feat: Add Order and Transaction views to stock API

--- stock/urls.py
+++ stock/urls.py
@@ -8,12 +8,16 @@ from stock.views import ProductViewSet
 from stock.views import CategoryViewSet
 from stock.views import SupplierViewSet
 from stock.views import ManufacturerViewSet
+from stock.views import OrderViewSet
+from stock.views import TransactionViewSet
 
 router = DefaultRouter()
 router.register('products', ProductViewSet)
 router.register('categories', CategoryViewSet)
 router.register('suppliers', SupplierViewSet)
 router.register('manufacturers', ManufacturerViewSet)
+router.register('orders', OrderViewSet)
+router.register('transactions', TransactionViewSet)
 
 urlpatterns = [
     path('', include(router.urls))
