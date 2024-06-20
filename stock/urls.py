d6713fffed71fe708ecd8714db3b7a021b9f2ed8
Omar Flores <sazardev@gmail.com>
2024-06-20 12:22:45 -0700

    feat: Add login functionality and user-related views and endpoints

--- stock/urls.py
+++ stock/urls.py
@@ -2,23 +2,27 @@
 from django.urls import include
 from django.urls import path
 
-from rest_framework.routers import DefaultRouter
-
+from stock.views import LoginView
+from stock.views import UserViewSet
+from stock.views import OrderViewSet
 from stock.views import ProductViewSet
 from stock.views import CategoryViewSet
 from stock.views import SupplierViewSet
-from stock.views import ManufacturerViewSet
-from stock.views import OrderViewSet
 from stock.views import TransactionViewSet
+from stock.views import ManufacturerViewSet
+
+from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
-router.register('products', ProductViewSet)
-router.register('categories', CategoryViewSet)
-router.register('suppliers', SupplierViewSet)
-router.register('manufacturers', ManufacturerViewSet)
+router.register('users', UserViewSet)
 router.register('orders', OrderViewSet)
+router.register('products', ProductViewSet)
+router.register('suppliers', SupplierViewSet)
+router.register('categories', CategoryViewSet)
 router.register('transactions', TransactionViewSet)
+router.register('manufacturers', ManufacturerViewSet)
 
 urlpatterns = [
+    path('login/', LoginView.as_view(), name='login'),
     path('', include(router.urls))
 ]
