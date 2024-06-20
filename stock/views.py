1a554780b6bd0902b84851f69b7ace3622d04761
Omar Flores <sazardev@gmail.com>
2024-06-20 12:22:50 -0700

    feat: Update authentication and permissions for user-related views and endpoints

--- stock/views.py
+++ stock/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render
 from rest_framework import viewsets
+from django.contrib.auth.models import User
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.authentication import TokenAuthentication
+
+from django.contrib.auth import authenticate
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
 
 from stock.models import Product
 from stock.models import Category
@@ -16,45 +24,127 @@ from stock.filters import ManufacturerFilter
 from stock.filters import OrderFilter
 from stock.filters import TransactionFilter
 
-from stock.serializers import CategorySerializer
+from stock.serializers import CategorySerializer, UserSerializer
 from stock.serializers import ManufacturerSerializer
 from stock.serializers import ProductSerializer
 from stock.serializers import SupplierSerializer
 from stock.serializers import OrderSerializer
 from stock.serializers import TransactionSerializer
 
+class LoginView(APIView):
+    def post(self, request):
+        username = request.data.get("username")
+        password = request.data.get("password")
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            token, created = Token.objects.get_or_create(user=user)
+            return Response({"token": token.key})
+        else:
+            return Response({"error": "Wrong Credentials"}, status=status.HTTP_400_BAD_REQUEST)
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    
+    def get_permissions(self):
+        if self.action == 'create':
+            self.permission_classes = []
+        else:
+            self.permission_classes = [IsAuthenticated]
+        return super(self.__class__, self).get_permissions()
+
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_class = ProductFilter
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        if self.request.user.is_authenticated:
+            return Product.objects.filter(user=self.request.user)
+        return Product.objects.none()
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
     filter_backends = [DjangoFilterBackend]
     filterset_class = CategoryFilter
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        if self.request.user.is_authenticated:
+            return Category.objects.filter(user=self.request.user)
+        return Category.objects.none()
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
 
 class SupplierViewSet(viewsets.ModelViewSet):
     queryset = Supplier.objects.all()
     serializer_class = SupplierSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_class = SupplierFilter
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        if self.request.user.is_authenticated:
+            return Supplier.objects.filter(user=self.request.user)
+        return Supplier.objects.none()
+    
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
 
 class ManufacturerViewSet(viewsets.ModelViewSet):
-    queryset = Manufacturer.objects.all()
+    queryset = Manufacturer.objects.all().order_by('id')
     serializer_class = ManufacturerSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_class = ManufacturerFilter
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        if self.request.user.is_authenticated:
+            return Supplier.objects.filter(user=self.request.user)
+        return Supplier.objects.none()
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
 
 class OrderViewSet(viewsets.ModelViewSet):
-    queryset = Order.objects.all()
+    queryset = Order.objects.all().order_by('-created_at')
     serializer_class = OrderSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_class = OrderFilter
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        if self.request.user.is_authenticated:
+            return Order.objects.filter(user=self.request.user)
+        return Order.objects.none()
+    
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
 
 class TransactionViewSet(viewsets.ModelViewSet):
-    queryset = Transaction.objects.all()
+    queryset = Transaction.objects.all().order_by('-created_at')
     serializer_class = TransactionSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_class = TransactionFilter
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        if self.request.user.is_authenticated:
+            return Transaction.objects.filter(user=self.request.user)
+        return Transaction.objects.none()
+    
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
