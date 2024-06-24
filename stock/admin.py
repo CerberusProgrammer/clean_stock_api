6735c864bd30c62bb50d44bb0e0694e766a09614
Omar Flores <sazardev@gmail.com>
2024-06-24 09:54:56 -0700

    chore: Register Promotion model in admin.py and update site headers

--- stock/admin.py
+++ stock/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 
-from .models import Order, Product, Category, Manufacturer, Supplier, Transaction
+from .models import Order, Product, Category, Manufacturer, Promotion, Supplier, Transaction
 
+admin.site.site_header = 'Stock Management System'
+admin.site.site_title = 'Stock Management System'
+admin.site.index_title = 'Stock Management System'
+
+admin.site.register(Promotion)
 admin.site.register(Product)
 admin.site.register(Category)
 admin.site.register(Manufacturer)
